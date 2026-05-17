9c6afd1c7c7b0747214974081ed0c1d40db33e5d
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-05-17 22:12:58 +0530

    Time: 2 ms (82.42%), Space: 52.1 MB (51.59%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,27 +1,21 @@
 class Solution {
-    public boolean canReach(int[] arr, int start) {
-        int n = arr.length;
+    int n;
 
-        boolean[] visited = new boolean[n];
-        Queue<Integer> q = new ArrayDeque<>();
-
-        q.offer(start);
-
-        while (!q.isEmpty()) {
-            int i = q.poll();
-
-            if (i < 0 || i >= n || visited[i])
-                continue;
-
-            if (arr[i] == 0)
-                return true;
-
-            visited[i] = true;
-
-            q.offer(i + arr[i]);
-            q.offer(i - arr[i]);
+    private boolean dfs(int[] arr, int i) {
+        if(i < 0 || i >= n || arr[i] < 0) {
+            return false;
         }
+        if(arr[i] == 0)
+            return true;
 
-        return false;
+        arr[i] *= -1;
+        boolean left  = dfs(arr, i - arr[i]);
+        boolean right = dfs(arr, i + arr[i]);
+        return left || right;
+    }
+
+    public boolean canReach(int[] arr, int start) {
+        n = arr.length;
+        return dfs(arr, start);
     }
 }
