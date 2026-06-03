1d86195049e534d36e73a31784ef4ecf7b1633f5
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-06-03 18:33:09 +0530

    Time: 2 ms (100%), Space: 92.1 MB (39.66%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,21 +1,25 @@
 class Solution {
-    final static int MAX = 300005;
 
-    public int earliestFinishTime(int[] la, int[] lb, int[] wa, int[] wb) {
-        int l = MAX, w = MAX, minL = MAX, minW = MAX;
-        int n = la.length, m = wa.length;
+    private int findFinishTime(int[] start1, int[] duration1, int[] start2, int[] duration2) {
 
-        for (int i = 0; i < n; ++i)
-            l = Math.min(l, la[i] + lb[i]);
-
-        for (int i = 0; i < m; ++i) {
-            w = Math.min(w, wa[i] + wb[i]);
-            minL = Math.min(minL, Math.max(wa[i], l) + wb[i]);
+        int finish1 = Integer.MAX_VALUE;
+        for (int i = 0; i < start1.length; i++) {
+            finish1 = Math.min(finish1, start1[i] + duration1[i]);
         }
 
-        for (int i = 0; i < n; ++i)
-            minW = Math.min(minW, Math.max(la[i], w) + lb[i]);
+        int finish2 = Integer.MAX_VALUE;
+        for (int i = 0; i < start2.length; i++) {
+            finish2 = Math.min(finish2, Math.max(finish1, start2[i]) + duration2[i]);
+        }
 
-        return Math.min(minW, minL);
+        return finish2;
+    }
+
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int pehleLand_FirWater = findFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
+
+        int pehleWater_FirLand = findFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
+
+        return Math.min(pehleLand_FirWater, pehleWater_FirLand);
     }
 }
