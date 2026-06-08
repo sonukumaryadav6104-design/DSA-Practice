44e06a3f30936222f699ba33f9fa451d39dd1874
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-06-08 16:30:37 +0530

    Time: 67 ms (62.16%), Space: 86.1 MB (50.31%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -1,47 +1,34 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
-        Map<Integer,TreeNode>map=new HashMap<>();
-        Map<Integer,Integer> root=new HashMap<>();
-        for(int[]arr:descriptions){
-            int parent=arr[0];
-            int child=arr[1];
-            boolean isLeft=arr[2]==1?true:false;
-            if(!map.containsKey(parent)){
-                map.put(parent,new TreeNode(parent));
+        Map<Integer, TreeNode> mp = new HashMap<>();
+        Set<Integer> childSet = new HashSet<>();
+
+        
+        for (int[] description : descriptions) {
+            int parent = description[0];
+            int child = description[1];
+            boolean isLeft = description[2] == 1;
+
+            mp.putIfAbsent(parent, new TreeNode(parent));
+            mp.putIfAbsent(child, new TreeNode(child));
+
+            if (isLeft) {
+                mp.get(parent).left = mp.get(child);
+            } else {
+                mp.get(parent).right = mp.get(child);
             }
-            if(!map.containsKey(child)){
-                map.put(child,new TreeNode(child));
-            }
-            if(isLeft){
-                map.get(parent).left=map.get(child);
-            }else map.get(parent).right=map.get(child);
-            if(root.getOrDefault(parent,0)!=-1){
-                root.put(parent,1);
-            }
-            root.put(child,-1);
+
+            childSet.add(child);
         }
-        int rootValue=0;
-        for(var entry:root.entrySet()){
-            if(entry.getValue()==1){
-                rootValue=entry.getKey();
-                break;
+
+       
+        for (int[] description : descriptions) {
+            int parent = description[0];
+            if (!childSet.contains(parent)) {
+                return mp.get(parent);
             }
         }
-        return map.get(rootValue);
+
+        return null;
     }
 }
