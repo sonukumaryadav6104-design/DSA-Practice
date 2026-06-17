c3c7352f73ca9942aa8aae1772dea263bfefebf9
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-06-17 12:00:42 +0530

    Time: 4 ms (76.3%), Space: 55.7 MB (12.59%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,33 +1,17 @@
-class Solution {
+public class Solution {
+
     public String processStr(String s) {
         StringBuilder result = new StringBuilder();
-
-        int n = s.length();
-        StringBuilder st = new StringBuilder();
-
-        for(int i = 0;i<n;i++){
-            st.append(s.charAt(i));
-        }
-        String sb= st.toString();
-
-        for(int i = 0;i<n;i++){
-           String charAtI = s.substring(i, i + 1); 
-            if (sb.contains(charAtI) && s.charAt(i) != '*' && s.charAt(i) != '#' && s.charAt(i) != '%') {
-                result.append(s.charAt(i));
-            }
-
-           else if (s.charAt(i) == '*' && result.length() > 0) {
-                result.setLength(result.length() - 1);
-
-           }
-
-           else if (s.charAt(i) == '#') {
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                if (result.length() > 0) result.deleteCharAt(result.length() - 1);
+            } else if (ch == '#') {
                 result.append(result.toString());
-           } 
-           else if(s.charAt(i)=='%'){
-            result.reverse();
-           }
-           
+            } else if (ch == '%') {
+                result.reverse();
+            } else {
+                result.append(ch);
+            }
         }
         return result.toString();
     }
