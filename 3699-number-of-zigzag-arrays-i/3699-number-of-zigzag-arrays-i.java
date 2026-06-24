75d44fe9fee413c3d2437a31027e2c0fe93f3541
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-06-24 16:20:09 +0530

    Time: 1957 ms (5.13%), Space: 280.4 MB (7.69%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
@@ -1,34 +1,41 @@
-public class Solution {
-
-    private static final int MOD = 1_000_000_007;
+class Solution {
+    int MOD = 1_000_000_007;
 
     public int zigZagArrays(int n, int l, int r) {
-        int[] dp0 = new int[r + 1];
-        int[] dp1 = new int[r + 1];
-        int[] sum0 = new int[r + 2];
-        int[] sum1 = new int[r + 2];
+        int N = n;
+        int M = r - l + 1;
 
-        for (int i = l; i <= r; i++) {
-            dp0[i] = 1;
-            dp1[i] = 1;
-            sum0[i] = i - l + 1;
-            sum1[i] = i - l + 1;
+        long[][][] t = new long[N + 1][M + 1][2];
+
+        
+        for (int prevVal = 1; prevVal <= M; prevVal++) {
+            t[N][prevVal][0] = 1;
+            t[N][prevVal][1] = 1;
         }
 
-        for (int i = 1; i < n; i++) {
-            for (int j = l; j <= r; j++) {
-                dp0[j] = (sum1[r] - sum1[j] + MOD) % MOD;
-                dp1[j] = sum0[j - 1];
+        for (int i = N - 1; i >= 0; i--) {
+
+            long[] prefDir0 = new long[M + 1];
+            long[] prefDir1 = new long[M + 1];
+
+            for (int prevVal = 1; prevVal <= M; prevVal++) {
+                prefDir0[prevVal] = (prefDir0[prevVal - 1] + t[i + 1][prevVal][0]) % MOD;
+                prefDir1[prevVal] = (prefDir1[prevVal - 1] + t[i + 1][prevVal][1]) % MOD;
             }
 
-            sum0[l] = dp0[l];
-            sum1[l] = dp1[l];
-            for (int j = l + 1; j <= r; j++) {
-                sum0[j] = (sum0[j - 1] + dp0[j]) % MOD;
-                sum1[j] = (sum1[j - 1] + dp1[j]) % MOD;
+            for (int prevVal = 1; prevVal <= M; prevVal++) {
+                t[i][prevVal][1] = (prefDir0[M] - prefDir0[prevVal] + MOD) % MOD;
+                t[i][prevVal][0] = prefDir1[prevVal - 1];
             }
         }
 
-        return (sum0[r] + sum1[r]) % MOD;
+        long result = 0;
+        for (int startVal = 1; startVal <= M; startVal++) {
+           
+            result = (result + t[1][startVal][1]) % MOD;
+            
+            result = (result + t[1][startVal][0]) % MOD;
+        }
+        return (int) result;
     }
 }
