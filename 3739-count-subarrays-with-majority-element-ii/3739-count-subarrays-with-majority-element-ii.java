334d448b932b14ad23e557990af95f37dfb3a47b
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-06-26 15:24:53 +0530

    Time: 23 ms (44.26%), Space: 111.7 MB (26.23%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public long countMajoritySubarrays(int[] nums, int target) {
-        //Instead of map you can use an array of size 2*n+1 as well because we can have cumulative sum from -n to +n
-        Map<Integer, Integer> mp = new HashMap<>();   // mp[cumSum] = frequency of how many times cumSum has occurred
+        
+        Map<Integer, Integer> mp = new HashMap<>();  
 
         int currSum = 0;
 
-        mp.put(0, 1); //we have seen cumSum = 0 in the beginning once
+        mp.put(0, 1); 
         long validLeftPoints = 0;
         long result = 0;
         for (int x : nums) {
