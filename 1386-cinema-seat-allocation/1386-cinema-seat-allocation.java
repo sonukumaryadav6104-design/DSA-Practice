b21a7f07bd691a87bb74709d87721cc8ee7df953
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-08-19 14:48:00 +0530

    Update 1386-cinema-seat-allocation.java

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,31 +1,31 @@
-// class Solution {
-//     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-//         Map<Integer, Set<Integer>> mp = new HashMap<>(); //row -> seats booked in each row
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        Map<Integer, Set<Integer>> mp = new HashMap<>(); 
 
-//         for(int[] reservedSeat : reservedSeats) {
-//             int row  = reservedSeat[0];
-//             int seat = reservedSeat[1];
-//             mp.computeIfAbsent(row, k -> new HashSet<>()).add(seat);
-//         }
+        for(int[] reservedSeat : reservedSeats) {
+            int row  = reservedSeat[0];
+            int seat = reservedSeat[1];
+            mp.computeIfAbsent(row, k -> new HashSet<>()).add(seat);
+        }
 
-//         int result = (n - mp.size()) * 2;
+        int result = (n - mp.size()) * 2;
 
-//         for(Map.Entry<Integer, Set<Integer>> entry : mp.entrySet()) {
-//             Set<Integer> bookedSeats = entry.getValue();
+        for(Map.Entry<Integer, Set<Integer>> entry : mp.entrySet()) {
+            Set<Integer> bookedSeats = entry.getValue();
 
-//             boolean groupA = !bookedSeats.contains(2) && !bookedSeats.contains(3) && !bookedSeats.contains(4) && !bookedSeats.contains(5);
-//             boolean groupB = !bookedSeats.contains(4) && !bookedSeats.contains(5) && !bookedSeats.contains(6) && !bookedSeats.contains(7);
-//             boolean groupC = !bookedSeats.contains(6) && !bookedSeats.contains(7) && !bookedSeats.contains(8) && !bookedSeats.contains(9);
+            boolean groupA = !bookedSeats.contains(2) && !bookedSeats.contains(3) && !bookedSeats.contains(4) && !bookedSeats.contains(5);
+            boolean groupB = !bookedSeats.contains(4) && !bookedSeats.contains(5) && !bookedSeats.contains(6) && !bookedSeats.contains(7);
+            boolean groupC = !bookedSeats.contains(6) && !bookedSeats.contains(7) && !bookedSeats.contains(8) && !bookedSeats.contains(9);
 
-//             if(groupA && groupC)
-//                 result += 2;
-//             else if(groupA || groupB || groupC)
-//                 result += 1;
-//         }
+            if(groupA && groupC)
+                result += 2;
+            else if(groupA || groupB || groupC)
+                result += 1;
+        }
 
-//         return result;
-//     }
-// }
+        return result;
+    }
+}
 
 
 
