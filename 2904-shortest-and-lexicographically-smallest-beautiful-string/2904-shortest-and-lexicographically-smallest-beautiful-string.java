6c554d92bf9dbb6ec933b7f2ff41f3fe1044ff19
Sonu kumar yadav <sonukumaryadav6104@gmail.com>
2026-08-31 09:00:19 +0530

    Time: 7 ms (11.11%), Space: 47 MB (5.93%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,25 +1,59 @@
+// Brute forces
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int n = s.length();
         for (int len = k; len <= n; len++) {
             String result = "";
             for (int start = 0; start + len <= n; start++) { 
+
                 String temp = s.substring(start, start + len); 
                 int ones = 0;
                 for (char ch : temp.toCharArray()) {
                     ones += (ch == '1') ? 1 : 0;
                 }
-                
+               
                 if (ones == k) {
                     if (result.isEmpty() || temp.compareTo(result) < 0)
                         result = temp;
                 }
             }
 
-            
+           
             if (!result.isEmpty())
                 return result;
         }
         return "";
     }
 }
+
+
+// (Sliding Window)
+
+
+// class Solution {
+//     public String shortestBeautifulSubstring(String s, int k) {
+//         int n = s.length();
+//         int i = 0;
+//         int ones = 0;     
+//         String result = "";
+//         for (int j = 0; j < n; j++) {
+//             if (s.charAt(j) == '1')
+//                 ones++;
+          
+//             while (ones > k || s.charAt(i) == '0') {
+//                 if (s.charAt(i) == '1')
+//                     ones--;
+//                 i++;
+//             }
+//             if (ones == k) {
+//                 String temp = s.substring(i, j + 1);
+//                 if (result.isEmpty()
+//                     || j - i + 1 < result.length()
+//                     || (j - i + 1 == result.length() && temp.compareTo(result) < 0)) {
+//                     result = temp;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
